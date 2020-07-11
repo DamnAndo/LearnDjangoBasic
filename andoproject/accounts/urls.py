47286edd84dca5452da1582b3a0cb2ba50e8f36e
jeremiando <situmeang92.fs@gmail.com>
2020-07-11 22:26:47 +0700

    Practice Login and Logout

--- andoproject/accounts/urls.py
+++ andoproject/accounts/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls import handler404,handler500
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('signup',views.signup,name='signup')
+    path('signup',views.signup,name='signup'),
+    path('',include('django.contrib.auth.urls'))
 ]
 
 # handler404 = 'views.not_found'
